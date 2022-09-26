de6a9eeec7c66124d236d09cabdf88e1da35f74b
Ghițun Iulian Bogdan <ghitun.iulian@yahoo.com>
2022-09-26 21:35:57 +0300

    added query selector helper functions

--- common/fetch.js
+++ common/fetch.js
@@ -1,1 +1,12 @@
+// common functions
+function $(selector) {
+    return document.querySelector(selector);
+} // query selector helper function
+
+function $$(selector) {
+    return document.querySelectorAll(selector);
+} // query selector all helper function, returns array of elements
+
+const div = () => document.createElement("dic"); // create div shortcut
+
 // common fetch and request js
