f8f5c047a5998deb2a27b25ba30cd72ec6ea6f5c
Ghitun Iulian Bogdan <ghitun.iulian@yahoo.com>
2022-10-12 16:51:01 +0300

    changed demo mode to Matei's way

--- common/fetch.js
+++ common/fetch.js
@@ -1,78 +1,113 @@
-let demo, fetch_adress;
+const API = {
+    CREATE: {
+        URL: "http://localhost:3000/recipes-json/create",
+        METHOD: "POST",
+    },
+    READ: {
+        URL: "http://localhost:3000/recipes-json",
+        METHOD: "GET",
+    },
+    UPDATE: {
+        URL: "http://localhost:3000/recipes-json/update",
+        METHOD: "PUT",
+    },
+    DELETE: {
+        URL: "http://localhost:3000/recipes-json/delete",
+        METHOD: "DELETE",
+    },
+};
 
-async function checkServer() {
-    try {
-        await fetch("http://localhost:3000/recipes-json");
-        demo = false;
-        fetch_adress = "http://localhost:3000/recipes-json";
-        console.log("SERVER MODE");
-    } catch (err) {
-        demo = true;
-        fetch_adress = "../data/recipes.json";
-        console.log("DEMO MODE");
-    }
-    loadRecipes();
+let editId;
+//const demo = location.host === "qbogdan.github.io" ? true : false;
+demo = true;
+
+const inLineChanges = demo;
+if (demo) {
+    API.READ.URL = "../data/recipes.json";
+    API.DELETE.URL = "../data/delete.json";
+    API.CREATE.URL = "../data/create.json";
+    API.UPDATE.URL = "../data/update.json";
+
+    API.DELETE.METHOD = "GET";
+    API.CREATE.METHOD = "GET";
+    API.UPDATE.METHOD = "GET";
 }
 
-checkServer();
-
 function loadRecipes() {
-    fetch(fetch_adress)
-        .then(list => list.json())
-        .then(r => {
+    fetch(API.READ.URL)
+        .then((list) => list.json())
+        .then((r) => {
             recipes = r;
+            //reload display
         });
 }
 
 function createRecipe(recipe) {
-    if (demo) {
-        const date = new Date();
-        recipe.id = `demoID${date.getTime()}`;
-        recipes.push(recipe);
-    } else {
-        return fetch("http://localhost:3000/recipes-json/create", {
-            method: "POST",
-            headers: {
-                "Content-Type": "application/json",
-            },
-            body: JSON.stringify(recipe),
-        });
-    }
-}
+    const method = API.CREATE.METHOD;
+    const date = new Date();
 
-function testDisplay() {
-    recipes.forEach(recipe => {
-        console.log(recipe.name, " - id: ", recipe.id);
-    });
-}
-
-function deleteRecipe(id) {
-    if (demo) {
-        recipes = recipes.filter(recipe => recipe.id !== id);
-    } else {
-        return fetch("http://localhost:3000/recipes-json/delete", {
-            method: "DELETE",
-            headers: {
-                "Content-Type": "application/json",
-            },
-            body: JSON.stringify({ id: id }),
+    fetch(API.CREATE.URL, {
+        method,
+        headers: {
+            "Content-Type": "application/json",
+        },
+        body: method === "GET" ? null : JSON.stringify(recipe),
+    })
+        .then((res) => res.json())
+        .then((r) => {
+            if (r.success) {
+                if (inLineChanges) {
+                    recipe.id = `demoID${date.getTime()}`;
+                    recipes.push(recipe);
+                } else {
+                    loadRecipes();
+                }
+            }
         });
-    }
 }
 
 function updateRecipe(recipe) {
-    if (demo) {
-        const update = recipes.find(r => r.id === recipe.id);
-        for (let key in update) {
-            update[key] = recipe[key];
-        }
-    } else {
-        return fetch("http://localhost:3000/recipes-json/update", {
-            method: "PUT",
-            headers: {
-                "Content-Type": "application/json",
-            },
-            body: JSON.stringify(update),
+    const method = API.UPDATE.METHOD;
+
+    fetch(API.UPDATE.URL, {
+        method,
+        headers: {
+            "Content-Type": "application/json",
+        },
+        body: method === "GET" ? null : JSON.stringify(recipe),
+    })
+        .then((res) => res.json())
+        .then((r) => {
+            if (r.success) {
+                if (inLineChanges) {
+                    recipes = recipes.map((r) => (r.id === editId ? recipe : r));
+                    // reload display
+                } else {
+                    loadRecipes();
+                }
+            }
         });
-    }
+}
+
+function deleteRecipe(id) {
+    const method = API.UPDATE.METHOD;
+
+    return fetch(API.DELETE.URL, {
+        method,
+        headers: {
+            "Content-Type": "application/json",
+        },
+        body: method === "GET" ? null : JSON.stringify({ id }),
+    })
+        .then((res) => res.json())
+        .then((r) => {
+            if (r.success) {
+                if (inLineChanges) {
+                    recipes = recipes.filter((r) => r.id !== id);
+                    // reload display
+                } else {
+                    loadRecipes();
+                }
+            }
+        });
 }
