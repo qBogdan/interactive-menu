063cf72b23b2a88cd0f0602dcb2733569c697d26
Ichim Emanuel <ichim.emy24@gmail.com>
2022-10-24 17:40:30 +0300

    show active button

--- admin/main.js
+++ admin/main.js
@@ -1,15 +1,28 @@
 function adminInit(recipes, categories) {
-    displayCategories(categories);
+  displayCategories(categories);
 }
 
 function displayCategories(categories) {
-    categories.forEach(cat => {
-        const thisCat = div();
-        thisCat.classList.add("category");
-        thisCat.innerText = cat;
-        thisCat.dataset.category = cat;
-        $(".categories").append(thisCat);
-    });
+  categories.forEach((cat) => {
+    const thisCat = div();
+    thisCat.classList.add("category");
+    thisCat.innerText = cat;
+    thisCat.dataset.category = cat;
+    $(".categories").append(thisCat);
+  });
 }
+function initEvents() {
+  document.getElementById("categories").addEventListener("click", function (e) {
+    if (e.target.matches("a")) {
+      var id = e.target.getAttribute("data-category");
+      showPage(id);
+    }
+  });
+}
+document.getElementById("category").addEventListener("click", function () {
+  if (this.class.contains("active")) {
+    this.class.remove("active");
+  } else this.class.add("active");
+});
 
 adminInit();
